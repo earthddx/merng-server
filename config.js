796657a15aa394293f09205a38b860562c6e043a
Artem Murzo <47094239+earthddx@users.noreply.github.com>
2020-02-20 20:49:09 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports ={
+  MONGODB: 'mongodb+srv://earthddx:sCGLciZPPrYeCDD4@cluster0-5ksf8.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+}
+//<password>-paste in password from atlas
+//instead of test paste in database name{merng}
